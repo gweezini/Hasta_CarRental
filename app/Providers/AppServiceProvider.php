77eb64b8b7ff3366e60733b868396f3ba3b43157
GWEE ZI NI <gwee@graduate.utm.my>
2026-01-03 03:51:29 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Vehicle;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        View::composer('layouts.admin', function ($view) {
+            
+            $roadTaxAlerts = Vehicle::whereDate('road_tax_expiry', '<=', Carbon::now()->addDays(30))->get();
+
+            $insuranceAlerts = Vehicle::whereDate('insurance_expiry', '<=', Carbon::now()->addDays(30))->get();
+
+            $view->with('roadTaxAlerts', $roadTaxAlerts)
+                 ->with('insuranceAlerts', $insuranceAlerts);
+        });
     }
 }
