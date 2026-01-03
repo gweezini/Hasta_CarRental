79c01a84347561bb5c3ef43d0ece4c2a4ac155e3
GWEE ZI NI <gwee@graduate.utm.my>
2026-01-03 13:51:08 +0800

    Create 2026_01_03_134850_adjust_users_table_fields.php

---
+++ database/migrations/2026_01_03_134850_adjust_users_table_fields.php
@@ -0,0 +1,20 @@
+public function up(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        if (Schema::hasColumn('users', 'staff_id')) {
+            $table->dropColumn('staff_id');
+        }
+
+        if (!Schema::hasColumn('users', 'account_holder')) {
+            $table->string('account_holder')->nullable()->after('account_number');
+        }
+    });
+}
+
+public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('staff_id')->nullable();
+        $table->dropColumn('account_holder');
+    });
+}
