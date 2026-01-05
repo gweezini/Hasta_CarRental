e2cdc15df28773f18f3403789a5c7fd0593a244d
michellehochiaseed <michelledewdew0123@gmail.com>
2026-01-05 20:00:00 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -865,7 +865,7 @@
 
             <p class="card__price">
               Starting from
-              <strong>RM {{ number_format($vehicle->pricingTier ? $vehicle->pricingTier->rates->min('price') : $vehicle->price_per_hour, 2) }}</strong> / hour
+              <strong>RM {{ number_format($vehicle->pricingTier ? $vehicle->pricingTier->rules->min('price') : $vehicle->price_per_hour, 2) }}</strong> / hour
               @include('partials.price-modal', ['vehicle' => $vehicle])
             </p>
 
