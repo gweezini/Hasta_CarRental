035fc509014410ba62f239fdd729661e79d2d3c4
GWEE ZI NI <gwee@graduate.utm.my>
2026-01-07 21:43:35 +0800

    Update VehicleSeeder.php

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -12,18 +12,19 @@ class VehicleSeeder extends Seeder
      */
     public function run(): void
 {
+    // Added capacity to the array (index 10)
     $vehicles = [
-        ['V001', 'MCP 6113', 'PERODUA', 'AXIA', 2014, 1, 8, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg'],
-        ['V002', 'JQU 1957', 'PERODUA', 'AXIA', 2015, 1, 9, 'Unavailable', 1, 'PERODUA-Axia-5639_10.jpeg'],
-        ['V003', 'NDD 7803', 'PERODUA', 'AXIA', 2016, 1, 7, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg'],
-        ['V004', 'CEX 5224', 'PERODUA', 'AXIA', 2024, 1, 9, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg'],
-        ['V005', 'UTM 3365', 'PERODUA', 'AXIA', 2024, 1, 8, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg'],
-        ['V006', 'JPN 1416', 'PERODUA', 'MYVI', 2013, 1, 7, 'Available', 3, 'perodua-myvi-color-797792.png'],
-        ['V007', 'VC 6522', 'PERODUA', 'MYVI', 2016, 1, 6, 'Available', 3, 'perodua-myvi-color-797792.png'],
-        ['V008', 'UTM 3655', 'PERODUA', 'BEZZA', 2023, 1, 9, 'Available', 2, 'bezza.jpg'],
-        ['V009', 'UTM 3057', 'PERODUA', 'BEZZA', 2023, 1, 7, 'Available', 2, 'bezza.jpg'],
-        ['V010', 'QRP 5205', 'HONDA', 'DASH 125', 2021, 1, 5, 'Available', 5, 'DASH125.png'],
-        ['V011', 'JWD 9496', 'HONDA', 'BEAT 110', 2023, 1, 6, 'Available', 5, 'Honda_Beat110.png'],
+        ['V001', 'MCP 6113', 'PERODUA', 'AXIA', 2014, 1, 8, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg', 5],
+        ['V002', 'JQU 1957', 'PERODUA', 'AXIA', 2015, 1, 9, 'Unavailable', 1, 'PERODUA-Axia-5639_10.jpeg', 5],
+        ['V003', 'NDD 7803', 'PERODUA', 'AXIA', 2016, 1, 7, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg', 5],
+        ['V004', 'CEX 5224', 'PERODUA', 'AXIA', 2024, 1, 9, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg', 5],
+        ['V005', 'UTM 3365', 'PERODUA', 'AXIA', 2024, 1, 8, 'Available', 1, 'PERODUA-Axia-5639_10.jpeg', 5],
+        ['V006', 'JPN 1416', 'PERODUA', 'MYVI', 2013, 1, 7, 'Available', 3, 'perodua-myvi-color-797792.png', 5],
+        ['V007', 'VC 6522', 'PERODUA', 'MYVI', 2016, 1, 6, 'Available', 3, 'perodua-myvi-color-797792.png', 5],
+        ['V008', 'UTM 3655', 'PERODUA', 'BEZZA', 2023, 1, 9, 'Available', 2, 'bezza.jpg', 5],
+        ['V009', 'UTM 3057', 'PERODUA', 'BEZZA', 2023, 1, 7, 'Available', 2, 'bezza.jpg', 5],
+        ['V010', 'QRP 5205', 'HONDA', 'DASH 125', 2021, 1, 5, 'Available', 5, 'DASH125.png', 2],
+        ['V011', 'JWD 9496', 'HONDA', 'BEAT 110', 2023, 1, 6, 'Available', 5, 'Honda_Beat110.png', 2],
     ];
 
     foreach ($vehicles as $v) {
@@ -38,6 +39,7 @@ class VehicleSeeder extends Seeder
             'status' => $v[7],
             'type_id' => $v[8],
             'vehicle_image'=> $v[9],
+            'capacity' => $v[10], // Added capacity
         ]);
     }
 }
